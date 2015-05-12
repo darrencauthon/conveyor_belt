a87d781abb42651ef8bba36a433b104d3e65d579
Darren Cauthon <darren@cauthon.com>
2015-05-12 13:53:24 -0500

    No need for the @ here.

--- lib/conveyor_belt/in_memory_contract.rb
+++ lib/conveyor_belt/in_memory_contract.rb
@@ -16,11 +16,11 @@ module ConveyorBelt
     end
 
     def mark_for_ignoring target_id
-      @target_ids_to_ignore << target_id
+      target_ids_to_ignore << target_id
     end
 
     def mark_for_execution target_id
-      @target_ids_to_execute << target_id
+      target_ids_to_execute << target_id
     end
 
     def stop_mass_operation_definition mass_operation
