56c3d3f32e1c7000c22f926d5cbb281584bfbc45
Darren Cauthon <darren@cauthon.com>
2015-05-11 21:43:19 -0500

    Ignore.

--- lib/conveyor_belt/contract.rb
+++ lib/conveyor_belt/contract.rb
@@ -2,7 +2,7 @@ module ConveyorBelt
   class Contract
     #start_mass_operation_definition mass_operation
     #stop_mass_operation_definition mass_operation
-    #execute_single_target single_operation
-    #ignore_single_target single_operation
+    #execute_single_operation single_operation
+    #ignore_single_operation single_operation
   end
 end
