b9a5f0dc630a08d57434766eb3542c6ef1253d54
Darren Cauthon <darren@cauthon.com>
2015-05-12 13:55:07 -0500

    Drop the constructor.

--- lib/conveyor_belt/in_memory_contract.rb
+++ lib/conveyor_belt/in_memory_contract.rb
@@ -2,17 +2,18 @@ module ConveyorBelt
 
   class InMemoryContract < Contract
 
-    attr_reader :target_ids_to_execute
-    attr_reader :target_ids_to_ignore
     attr_reader :mass_operation
-
-    def initialize
-      @target_ids_to_execute = []
-      @target_ids_to_ignore  = []
-    end
 
     def start_mass_operation_definition mass_operation
       @mass_operation = mass_operation
+    end
+
+    def target_ids_to_execute
+      @target_ids_to_execute ||= []
+    end
+
+    def target_ids_to_ignore
+      @target_ids_to_ignore ||= []
     end
 
     def mark_for_ignoring target_id
