6e44520f7aeff26f7921ab4550d4e0443ab7c4a9
Darren Cauthon <darren@cauthon.com>
2015-05-11 22:01:27 -0500

    Store the ids that will be executed later.

--- lib/conveyor_belt/in_memory_contract.rb
+++ lib/conveyor_belt/in_memory_contract.rb
@@ -1,9 +1,20 @@
 module ConveyorBelt
   class InMemoryContract < Contract
+
+    attr_reader :target_ids
+
+    def initialize
+      @target_ids = []
+    end
+
     def start_mass_operation_definition _
     end
 
     def ignore_target _
+    end
+
+    def execute_target target_id
+      @target_ids << target_id
     end
   end
 end
--- spec/conveyor_belt/in_memory_contract_spec.rb
+++ spec/conveyor_belt/in_memory_contract_spec.rb
@@ -16,4 +16,13 @@ describe ConveyorBelt::InMemoryContract do
     end
   end
 
+  describe "execute target" do
+    it "should store the target id" do
+      id1, id2 = random_string, random_string
+      contract.execute_target id1
+      contract.execute_target id2
+      contract.target_ids.must_equal [id1, id2]
+    end
+  end
+
 end
