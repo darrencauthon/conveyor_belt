38361095815b472c464efb6d033431b6e7cefd3e
Darren Cauthon <darren@cauthon.com>
2015-05-11 21:40:15 -0500

    Stub methods I will have to define.

--- lib/conveyor_belt/contract.rb
+++ lib/conveyor_belt/contract.rb
@@ -1,4 +1,8 @@
 module ConveyorBelt
   class Contract
+    #start_mass_operation_definition mass_operation
+    #stop_mass_operation_definition mass_operation
+    #execute_single_target single_operation
+    #ignore_single_target single_operation
   end
 end
